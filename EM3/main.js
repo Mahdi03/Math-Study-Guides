f21111779e981abf63322185d3f77e9cd69bf775
Rohan Kumar <50250703+BroSkillsRo@users.noreply.github.com>
2019-10-04 14:09:32 -0700

    Made so input isn't in sigma tags

--- EM3/main.js
+++ EM3/main.js
@@ -63,10 +63,9 @@ document.body.onload = function() {
             //Added Sigma tags
             var sigmaTags = document.getElementsByTagName("sigma");
             for (var r = 0; r < sigmaTags.length; r++) {
-                var sigmaInput = sigmaTags[r].innerHTML;
-                var startValue = sigmaTags[r].getAttribute("start") ? sigmaTags[r].getAttribute("start") : "";
-                var endValue = sigmaTags[r].getAttribute("end") ? sigmaTags[r].getAttribute("end") : "";
-                sigmaTags[r].innerHTML = "<table class='sigmaTag' style='display: inline-table; transform: translateY(-30%);'><tr><td>" + endValue + "</td></tr><tr><td>&Sigma;</td><td>(" + sigmaInput + ")</td></tr><tr><td>"+ startValue + "</td></tr></table>";
+                var startValue = sigmaTags[r].getAttribute("start");
+                var endValue = sigmaTags[r].getAttribute("end");
+                sigmaTags[r].innerHTML = "<table class='sigmaTag' style='display: inline-table; transform: translateY(-30%);'><tr><td>" + endValue + "</td></tr><tr><td>&Sigma;</td></tr><tr><td>"+ startValue + "</td></tr></table>";
             }
             //Defined vector tag
             var vectorTags = document.getElementsByTagName("vector");
