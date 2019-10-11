fd3334bf3f11499953afe606e94b50b810eb4389
Mahdi03 <damahdi03@gmail.com>
2019-10-11 15:15:19 -0700

    working on it

--- countViews.js
+++ countViews.js
@@ -1,6 +1,23 @@
 /*getFile();
 getViewsFromFile();
 addViewsToFile();*/
+//Adds JQuery:
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+$(document).ready(function () {
+   $.ajax({
+       type: "GET",
+       url: "../../../views.xml",
+       dataType: "xml",
+       success: addTime
+   });
+});
+function addTime(xml) {
+    $(xml).find("")
+}
 var views = 0;
 var xhrGet = new XMLHttpRequest();
 xhrGet.onreadystatechange = function () {
