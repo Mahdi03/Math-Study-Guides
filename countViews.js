5aa4236ab9237c35b0903ad2c36835355436a648
Mahdi Ayman <damahdi03@gmail.com>
2019-10-11 22:25:26 -0700

    Testing out xhr

--- countViews.js
+++ countViews.js
@@ -1,12 +1,4 @@
-/*getFile();
-getViewsFromFile();
-addViewsToFile();*/
-//Adds JQuery:
-/*var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
-*/
+/*
 var views = 0;
 var xhrGet = new XMLHttpRequest();
 xhrGet.onreadystatechange = function () {
@@ -25,3 +17,7 @@ xhrPost.onreadystatechange = function () {
 }
 xhrPost.open("GET", "https://mahdi03.github.io/Math-Study-Guides/views.txt?views=" + views, true);
 xhrPost.send();
+*/
+    var xhrPost = new XMLHttpRequest();
+    xhrPost.open("GET", "https://mahdi03.github.io/Math-Study-Guides/views.txt?views=1", true);
+    xhrPost.send();
