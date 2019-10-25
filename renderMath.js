f8ba8992d8e2b755810749b3dff77c0024a4a72e
Mahdi Ayman <damahdi03@gmail.com>
2019-10-24 19:45:33 -0700

    Finished Off Unit 2, fixed spalling errors, added integral tag, added unit 3 file

--- renderMath.js
+++ renderMath.js
@@ -7,6 +7,7 @@ function addImportanceToFormulas() {
         });
     });
 }
+
 function removeImportanceFromElements() {
     var elementQueryList = ["sigma td"]; //Use CSS Selectors Here
     elementQueryList.forEach((elementQuery) => {
@@ -16,92 +17,105 @@ function removeImportanceFromElements() {
         });
     });
 }
+
 function renderMath() {
     //Replaced/Defined all math function 
-        //Defined all Sqrt Tags
-        
+    //Defined all Sqrt Tags
+
     var sqrtTags = document.getElementsByTagName("sqrt");
-        for (var e = 0; e < sqrtTags.length; e++) {
-            var sqrtInput = sqrtTags[e].innerHTML;
-            sqrtTags[e].innerHTML = "&radic;<span style='border-top: 0.5px solid black;'>" + sqrtInput + "</span>";
+    for (var e = 0; e < sqrtTags.length; e++) {
+        var sqrtInput = sqrtTags[e].innerHTML;
+        sqrtTags[e].innerHTML = "&radic;<span style='border-top: 0.5px solid black;'>" + sqrtInput + "</span>";
+    }
+    //Defined all Limit Tags
+    var limTags = document.getElementsByTagName("lim");
+    for (var f = 0; f < limTags.length; f++) {
+        var limAs = limTags[f].getAttribute("as");
+        var limApproaches = limTags[f].getAttribute("approaches");
+        limTags[f].innerHTML = "<div class='fraction'><div class='top'><var>lim</var></div><div class='bottom'><var>" + limAs + "</var>&rarr;" + limApproaches + "</div></div>";
+    }
+    //Defined trig tags
+    var sinTags = document.getElementsByTagName("sin");
+    for (var j = 0; j < sinTags.length; j++) {
+        var sinInput = sinTags[j].innerHTML;
+        sinTags[j].innerHTML = "sin(<var>" + sinInput + "</var>)";
+    }
+    var cosTags = document.getElementsByTagName("cos");
+    for (var k = 0; k < cosTags.length; k++) {
+        var cosInput = cosTags[k].innerHTML;
+        cosTags[k].innerHTML = "cos(<var>" + cosInput + "</var>)";
+    }
+    var tanTags = document.getElementsByTagName("tan");
+    for (var l = 0; l < tanTags.length; l++) {
+        var tanInput = tanTags[l].innerHTML;
+        tanTags[l].innerHTML = "tan(<var>" + tanInput + "</var>)";
+    }
+    var cscTags = document.getElementsByTagName("csc");
+    for (var m = 0; m < cscTags.length; m++) {
+        var cscInput = cscTags[m].innerHTML;
+        cscTags[m].innerHTML = "csc(<var>" + cscInput + "</var>)";
+    }
+    var secTags = document.getElementsByTagName("sec");
+    for (var n = 0; n < secTags.length; n++) {
+        var secInput = secTags[n].innerHTML;
+        secTags[n].innerHTML = "sec(<var>" + secInput + "</var>)";
+    }
+    var cotTags = document.getElementsByTagName("cot");
+    for (var o = 0; o < cotTags.length; o++) {
+        var cotInput = cotTags[o].innerHTML;
+        cotTags[o].innerHTML = "cot(<var>" + cotInput + "</var>)";
+    }
+    //Defined log tag and ln tag
+    var logTags = document.getElementsByTagName("log");
+    for (var p = 0; p < logTags.length; p++) {
+        var logInput = logTags[p].innerHTML;
+        if (logTags[p].getAttribute("base") !== null) {
+            var base = logTags[p].getAttribute("base");
+            logTags[p].innerHTML = "log<sub>" + base + "</sub>(<var>" + logInput + "</var>)";
+        } else {
+            logTags[p].innerHTML = "log(<var>" + logInput + "</var>)";
         }
-        //Defined all Limit Tags
-        var limTags = document.getElementsByTagName("lim");
-        for (var f = 0; f < limTags.length; f++) {
-            var limAs = limTags[f].getAttribute("as");
-            var limApproaches = limTags[f].getAttribute("approaches");
-            limTags[f].innerHTML = "<div class='fraction'><div class='top'><var>lim</var></div><div class='bottom'><var>" + limAs + "</var>&rarr;" + limApproaches + "</div></div>";
-        }
-        //Defined trig tags
-        var sinTags = document.getElementsByTagName("sin");
-        for (var j = 0; j < sinTags.length; j++) {
-            var sinInput = sinTags[j].innerHTML;
-            sinTags[j].innerHTML = "sin(<var>" + sinInput + "</var>)";
-        }
-        var cosTags = document.getElementsByTagName("cos");
-        for (var k = 0; k < cosTags.length; k++) {
-            var cosInput = cosTags[k].innerHTML;
-            cosTags[k].innerHTML = "cos(<var>" + cosInput + "</var>)";
-        }
-        var tanTags = document.getElementsByTagName("tan");
-        for (var l = 0; l < tanTags.length; l++) {
-            var tanInput = tanTags[l].innerHTML;
-            tanTags[l].innerHTML = "tan(<var>" + tanInput + "</var>)";
-        }
-        var cscTags = document.getElementsByTagName("csc");
-        for (var m = 0; m < cscTags.length; m++) {
-            var cscInput = cscTags[m].innerHTML;
-            cscTags[m].innerHTML = "csc(<var>" + cscInput + "</var>)";
-        }
-        var secTags = document.getElementsByTagName("sec");
-        for (var n = 0; n < secTags.length; n++) {
-            var secInput = secTags[n].innerHTML;
-            secTags[n].innerHTML = "sec(<var>" + secInput + "</var>)";
-        }
-        var cotTags = document.getElementsByTagName("cot");
-        for (var o = 0; o < cotTags.length; o++) {
-            var cotInput = cotTags[o].innerHTML;
-            cotTags[o].innerHTML = "cot(<var>" + cotInput + "</var>)";
-        }
-        //Defined log tag and ln tag
-        var logTags = document.getElementsByTagName("log");
-        for (var p = 0; p < logTags.length; p++) {
-            var logInput = logTags[p].innerHTML;
-            if (logTags[p].getAttribute("base") !== null) {
-                var base = logTags[p].getAttribute("base");
-                logTags[p].innerHTML = "log<sub>" + base + "</sub>(<var>" + logInput + "</var>)";
-            } else {
-                logTags[p].innerHTML = "log(<var>" + logInput + "</var>)";
-            }
-        }
-        var lnTags = document.getElementsByTagName("ln");
-        for (var q = 0; q < lnTags.length; q++) {
-            var lnInput = lnTags[q].innerHTML;
-            lnTags[q].innerHTML = "ln (<var>" + lnInput + "</var>)";
-        }
-        //Defined Derivative Tags (Will need to be changed further as we learn new concepts)
-        //<nDeriv of="f" respectTo="t"></nDeriv> === df/dt
-        var derivativeTags = document.getElementsByTagName("derivative");
-        for (var r = 0; r < derivativeTags.length; r++) {
-            var derivativeOf = derivativeTags[r].getAttribute("of") ? derivativeTags[r].getAttribute("of") : "";
-            var derivativeRespectTo = derivativeTags[r].getAttribute("respectTo") ? derivativeTags[r].getAttribute("respectTo") : "x";
-            derivativeTags[r].innerHTML = "<div class='fraction'><div class='top'>d<var>" + derivativeOf + "</var></div><div class='bottom'>d<var>" + derivativeRespectTo + "</var></div></div>";
-        }
-        
-        //Defined vector tag
-        var vectorTags = document.getElementsByTagName("vector");
-        for (var i = 0; i < vectorTags.length; i++) {
-            var vectorName = vectorTags[i].innerHTML;
-            vectorTags[i].innerHTML = '<div class="outer"><div class="inner"><var>&rarr;</var></div><div class="inner"><var>' + vectorName + '</var></div></div>';
-        }
-        //Added Sigma tags
-        var sigmaTags = document.getElementsByTagName("sigma");
-        for (var r = 0; r < sigmaTags.length; r++) {
-            var startValue = sigmaTags[r].getAttribute("start");
-            var endValue = sigmaTags[r].getAttribute("end");
-            sigmaTags[r].innerHTML = "<table class='sigmaTag' style='display: inline-table; transform: translateY(-30%);'><tr><td>" + endValue + "</td></tr><tr><td>&Sigma;</td></tr><tr><td>"+ startValue + "</td></tr></table>";
-        }
+    }
+    var lnTags = document.getElementsByTagName("ln");
+    for (var q = 0; q < lnTags.length; q++) {
+        var lnInput = lnTags[q].innerHTML;
+        lnTags[q].innerHTML = "ln (<var>" + lnInput + "</var>)";
+    }
+    //Defined Derivative Tags (Will need to be changed further as we learn new concepts)
+    //<nDeriv of="f" respectTo="t"></nDeriv> === df/dt
+    var derivativeTags = document.getElementsByTagName("derivative");
+    for (var r = 0; r < derivativeTags.length; r++) {
+        var derivativeOf = derivativeTags[r].getAttribute("of") ? derivativeTags[r].getAttribute("of") : "";
+        var derivativeRespectTo = derivativeTags[r].getAttribute("respectTo") ? derivativeTags[r].getAttribute("respectTo") : "x";
+        derivativeTags[r].innerHTML = "<div class='fraction'><div class='top'>d<var>" + derivativeOf + "</var></div><div class='bottom'>d<var>" + derivativeRespectTo + "</var></div></div>";
+    }
+
+    var integralTags = document.getElementsByTagName("integral");
+    for (var h = 0; h < integralTags.length; h++) {
+        var lowerBound = integralTags[h].getAttribute("lowerBound");
+        var upperBound = integralTags[h].getAttribute("upperBound");
+        var respectTo = integralTags[h].getAttribute("respectTo");
+        var span = document.createElement("span");
+        span.innerHTML = "<var>d" + respectTo + "</var>";
+        integralTags[h].innerHTML = "&int;<sub style='display: inline-block; transform: translateY(5px);'>" + lowerBound + "</sub><sup style='display: inline-block; transform: translateY(-5px);'>" + upperBound + "</sup>" + integralTags[h].innerHTML;
+        integralTags[h].parentNode.insertBefore(span, integralTags[h].nextSibling);
+    }
+
+    //Defined vector tag
+    var vectorTags = document.getElementsByTagName("vector");
+    for (var i = 0; i < vectorTags.length; i++) {
+        var vectorName = vectorTags[i].innerHTML;
+        vectorTags[i].innerHTML = '<div class="outer"><div class="inner"><var>&rarr;</var></div><div class="inner"><var>' + vectorName + '</var></div></div>';
+    }
+    //Added Sigma tags
+    var sigmaTags = document.getElementsByTagName("sigma");
+    for (var r = 0; r < sigmaTags.length; r++) {
+        var startValue = sigmaTags[r].getAttribute("start");
+        var endValue = sigmaTags[r].getAttribute("end");
+        sigmaTags[r].innerHTML = "<table class='sigmaTag' style='display: inline-table; transform: translateY(-30%);'><tr><td>" + endValue + "</td></tr><tr><td>&Sigma;</td></tr><tr><td>" + startValue + "</td></tr></table>";
+    }
 }
+
 function toggleDiv(id) {
     var element = document.querySelector(id);
     if (element.style.display === "block") {
