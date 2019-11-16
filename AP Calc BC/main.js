bca9df75b9756889cdb1631f95fb82ab7b605b86
Mahdi Ayman <damahdi03@gmail.com>
2019-11-16 15:50:34 -0800

    Tried to fix the graphs not loading on the actual page

--- AP Calc BC/main.js
+++ AP Calc BC/main.js
@@ -5,8 +5,15 @@ document.body.onload = function() {
         var fileRequest = new XMLHttpRequest();
         fileRequest.onreadystatechange = function() {
             if (this.readyState == 4) {
-                if (this.status == 200) { element.innerHTML = this.responseText.replace('<link href="styles.css" rel="stylesheet" />', '').replace('<script src="main.js" async></script>', ''); }
-                if (this.status == 404) { console.log("File not found!!"); }
+                if (this.status == 200) {
+                    var responseHTML = this.responseText.replace('<link href="styles.css" rel="stylesheet" />', '').replace('<script src="main.js" async></script>', '');
+                    element.innerHTML = responseHTML;
+                    var m = str.search("<script>");
+                    var n = str.search("<\/script>");
+                    eval(responseHTML.substr(m + 8, n - (m + 8))); //8 Is the Length of <script>
+                } else if (this.status == 404) {
+                    console.log("File not found!!");
+                }
             }
         }
         fileRequest.open("GET", fileLink, false);
