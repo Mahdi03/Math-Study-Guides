0295864068baf64a5230e04a419a6663c978662b
Mahdi Ayman <damahdi03@gmail.com>
2019-11-16 16:12:56 -0800

    Fixed A Variable Naming Error

--- AP Calc BC/main.js
+++ AP Calc BC/main.js
@@ -1,5 +1,4 @@
 document.body.onload = function() {
-    var responseHTML;
     //Added an HTML Include Before Everything else so that the HTML renders properly
     document.querySelectorAll("[htmlInclude]").forEach((element) => {
         var fileLink = element.getAttribute("htmlInclude");
@@ -7,8 +6,12 @@ document.body.onload = function() {
         fileRequest.onreadystatechange = function() {
             if (this.readyState == 4) {
                 if (this.status == 200) {
-                    responseHTML = this.responseText.replace('<link href="styles.css" rel="stylesheet" />', '').replace('<script src="main.js" async></script>', '');
+                    var responseHTML = this.responseText.replace('<link href="styles.css" rel="stylesheet" />', '').replace('<script src="main.js" async></script>', '');
                     element.innerHTML = responseHTML;
+                    var m = responseHTML.search("<script>");
+                    var n = responseHTML.search("<\/script>");
+                    alert(responseHTML.substr(m + 8, n - (m + 8)));
+                    eval(responseHTML.substr(m + 8, n - (m + 8))); //8 Is the Length of <script>
                 } else if (this.status == 404) {
                     console.log("File not found!!");
                 }
@@ -17,10 +20,6 @@ document.body.onload = function() {
         fileRequest.open("GET", fileLink, false);
         fileRequest.send();
     });
-    var m = str.search("<script>");
-    var n = str.search("<\/script>");
-    alert(responseHTML.substr(m + 8, n - (m + 8)));
-    eval(responseHTML.substr(m + 8, n - (m + 8))); //8 Is the Length of <script>
     renderMath();
     addImportanceToFormulas();
 };
