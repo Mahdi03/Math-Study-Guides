d90a5c0bbc1d641f0c4f8e7c4bd3c425b0b9a5fb
Mahdi Ayman <damahdi03@gmail.com>
2019-11-16 16:02:58 -0800

    Trying to fix the JS to render the graphs

--- AP Calc BC/main.js
+++ AP Calc BC/main.js
@@ -1,4 +1,5 @@
 document.body.onload = function() {
+    var responseHTML;
     //Added an HTML Include Before Everything else so that the HTML renders properly
     document.querySelectorAll("[htmlInclude]").forEach((element) => {
         var fileLink = element.getAttribute("htmlInclude");
@@ -6,12 +7,8 @@ document.body.onload = function() {
         fileRequest.onreadystatechange = function() {
             if (this.readyState == 4) {
                 if (this.status == 200) {
-                    var responseHTML = this.responseText.replace('<link href="styles.css" rel="stylesheet" />', '').replace('<script src="main.js" async></script>', '');
+                    responseHTML = this.responseText.replace('<link href="styles.css" rel="stylesheet" />', '').replace('<script src="main.js" async></script>', '');
                     element.innerHTML = responseHTML;
-                    var m = str.search("<script>");
-                    var n = str.search("<\/script>");
-                    alert(responseHTML.substr(m + 8, n - (m + 8)));
-                    eval(responseHTML.substr(m + 8, n - (m + 8))); //8 Is the Length of <script>
                 } else if (this.status == 404) {
                     console.log("File not found!!");
                 }
@@ -20,6 +17,10 @@ document.body.onload = function() {
         fileRequest.open("GET", fileLink, false);
         fileRequest.send();
     });
+    var m = str.search("<script>");
+    var n = str.search("<\/script>");
+    alert(responseHTML.substr(m + 8, n - (m + 8)));
+    eval(responseHTML.substr(m + 8, n - (m + 8))); //8 Is the Length of <script>
     renderMath();
     addImportanceToFormulas();
 };
