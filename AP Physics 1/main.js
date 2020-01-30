ad17ffaac9afe29e49b9f09d8cc3dc68fec89297
Mahdi Ayman <damahdi03@gmail.com>
2020-01-29 18:04:15 -0800

    made changes

---
+++ AP Physics 1/main.js
@@ -0,0 +1,27 @@
+document.body.onload = function() {
+    //Added an HTML Include Before Everything else so that the HTML renders properly
+    document.querySelectorAll("[htmlInclude]").forEach((element) => {
+        var fileLink = element.getAttribute("htmlInclude");
+        var fileRequest = new XMLHttpRequest();
+        fileRequest.onreadystatechange = function() {
+            if (this.readyState == 4) {
+                if (this.status == 200) {
+                    var responseHTML = this.responseText.replace('<link href="styles.css" rel="stylesheet" />', '').replace('<script src="main.js" async><\/script>', '');
+                    element.innerHTML = responseHTML;
+                    /*var m = responseHTML.search("");
+                    var n = responseHTML.search("<\/script>");
+                    console.log("M: " + m + "N: " + n);
+                    console.log(responseHTML);
+                    console.log(responseHTML.substr(m + 8, n - (m + 8)));
+                    eval(responseHTML.substr(m + 8, n - (m + 8))); //8 Is the Length of <script>*/
+                } else if (this.status == 404) {
+                    console.log("File " + fileLink + " not found!!");
+                }
+            }
+        }
+        fileRequest.open("GET", fileLink, false);
+        fileRequest.send();
+    });
+    renderMath();
+    addImportanceToFormulas();
+};
