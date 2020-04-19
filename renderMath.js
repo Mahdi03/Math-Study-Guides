a32546509c79dd9fccc7d52cd599af92f86bfb66
Mahdi Ayman <damahdi03@gmail.com>
2020-04-18 22:51:47 -0700

    Rearranged files

--- renderMath.js
+++ renderMath.js
@@ -150,3 +150,25 @@ function toggleSolution(id) {
         document.querySelector(id + "ToggleLink").innerHTML = "Hide Solution";
     }
 }
+function drawArrow(context, fromx, fromy, tox, toy) {
+    //variables to be used when creating the arrow
+    var headlen = 10;
+    var angle = Math.atan2(toy - fromy, tox - fromx);
+    //starting path of the arrow from the start square to the end square and drawing the stroke
+    context.beginPath();
+    context.moveTo(fromx, fromy);
+    context.lineTo(tox, toy);
+    context.stroke();
+    //starting a new path from the head of the arrow to one of the sides of the point
+    context.beginPath();
+    context.moveTo(tox, toy);
+    context.lineTo(tox - headlen * Math.cos(angle - Math.PI / 7), toy - headlen * Math.sin(angle - Math.PI / 7));
+    //path from the side point of the arrow, to the other side point
+    context.lineTo(tox - headlen * Math.cos(angle + Math.PI / 7), toy - headlen * Math.sin(angle + Math.PI / 7));
+    //path from the side point back to the tip of the arrow, and then again to the opposite side point
+    context.lineTo(tox, toy);
+    context.lineTo(tox - headlen * Math.cos(angle - Math.PI / 7), toy - headlen * Math.sin(angle - Math.PI / 7));
+    //draws the paths created above
+    context.stroke();
+    context.fill();
+}
