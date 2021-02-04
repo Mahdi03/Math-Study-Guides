bc6fd185ffe81c9de6b04c72a20f669f56c145f0
Mahdi03 <damahdi03@gmail.com>
2021-02-03 22:50:15 -0800

    Working on the loading symbols' timing

--- AP Calc BC/main.js
+++ AP Calc BC/main.js
@@ -7,11 +7,14 @@ document.body.onload = function() {
         var toggleButton = document.querySelector(toggleButtonQuerySelector);
 
         function getFile() {
-
+            //Give it a wait logo so that the user knows that the website isn't frozen
+            document.body.style.cursor = "wait";
+            toggleButton.innerHTML = loadingLogoHTML;
             var fileRequest = new XMLHttpRequest();
             fileRequest.onreadystatechange = function() {
-                    if (this.readyState == 4) {
-                        if (this.status == 200) {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        const renderingHTMLFunction = new Promise((resolve, reject) => {
                             var responseHTML = this.responseText.replace('<link href="styles.css" rel="stylesheet" />', '').replace('<script src="main.js" async><\/script>', '');
                             element.innerHTML = responseHTML;
                             toggleButton.removeEventListener("click", getFile);
@@ -19,23 +22,29 @@ document.body.onload = function() {
                             renderMath(toggleButtonQuerySelector.replace("ToggleButton", " "));
                             addImportanceToFormulas();
                             removeImportanceFromElements();
+                            resolve("Yay we worked");
+                        });
+                        renderingHTMLFunction.then((successMessage) => {
                             //End the wait cursor
-                            //document.body.style.cursor = "initial";
-                            //toggleButton.innerHTML = "&minus;";
-                            /*var m = responseHTML.search("");
-                            var n = responseHTML.search("<\/script>");
-                            console.log("M: " + m + "N: " + n);
-                            console.log(responseHTML);
-                            console.log(responseHTML.substr(m + 8, n - (m + 8)));
-                            eval(responseHTML.substr(m + 8, n - (m + 8))); //8 Is the Length of <script>*/
-                        } else if (this.status == 404) {
-                            console.log("File " + fileLink + " not found!!");
-                        }
+                            document.body.style.cursor = "initial";
+                            toggleButton.innerHTML = "&minus;";
+
+                        }).catch((errorMessage) => {
+                            console.error("This function isn't even called how did we get here???");
+                        });
+
+                        /*var m = responseHTML.search("");
+                        var n = responseHTML.search("<\/script>");
+                        console.log("M: " + m + "N: " + n);
+                        console.log(responseHTML);
+                        console.log(responseHTML.substr(m + 8, n - (m + 8)));
+                        eval(responseHTML.substr(m + 8, n - (m + 8))); //8 Is the Length of <script>*/
+                    } else if (this.status == 404) {
+                        console.log("File " + fileLink + " not found!!");
                     }
                 }
-                //Give it a wait logo so that the user knows that the website isn't frozen
-            document.body.style.cursor = "wait";
-            toggleButton.innerHTML = loadingLogoHTML;
+            }
+
             fileRequest.open("GET", fileLink, false);
             fileRequest.send();
         }
